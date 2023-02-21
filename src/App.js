c5adc5cedbf9a1e4ae5eaff6596bae38fb0dd2c2
Apurbo Kumar Anup <apurboka@gmail.com>
2023-02-21 22:45:12 +0600

    Update

---
+++ src/App.js
@@ -0,0 +1,20 @@
+function App() {
+  return (
+    <div className="w-screen h-screen p-10 bg-gray-100 text-slate-700">
+      <h1 className="max-w-md mx-auto text-center text2xl font-bold">
+        Simple Counter Application
+      </h1>
+      <div className="max-w-md mx-auto mt-10 space-y-5">
+      <div className="p-4 h-auto flex flex-col items-center justify-content-center space-y-5 bg-light rounded shadow">
+      <div className="text-2xl font-semibold">0</div>
+      <div className="flex space-x-3">
+      <button className="bg-red-400 text-white px-3 py-2 rounded shadow">Decrement</button>
+      <button className="bg-indigo-400 text-white px-3 py-2 rounded shadow">Increment</button>
+      </div>
+      </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
