881fbd50746c8b2dfbc591db9b0b09277229eb64
Apurbo Kumar Anup <apurboka@gmail.com>
2023-02-22 21:01:08 +0600

    Done

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,13 +1,24 @@
 import React, { useState } from "react";
 
 export default function Counter() {
-  const [count, setCount] = useState(0);
-  const increment = () => {
-    setCount((prevCount) => prevCount + 1);
-  };
-  const decrement = () => {
-    setCount((prevCount) => prevCount - 1);
-  };
+  const [count, setCount] = useState(0); // useState returns a pair. 'count' is the current state. 'setCount' is a function we can use to update the state.
+
+  function increment() {
+    //setCount(prevCount => prevCount+=1);
+    setCount(function (prevCount) {
+      return (prevCount += 1);
+    });
+  }
+
+  function decrement() {
+    setCount(function (prevCount) {
+      if (prevCount > 0) {
+        return (prevCount -= 1); 
+      } else {
+        return (prevCount = 0);
+      }
+    });
+  }
 
   return (
     <div className="p-4 h-auto flex flex-col items-center justify-content-center space-y-5 bg-light rounded shadow">
@@ -20,7 +31,6 @@ export default function Counter() {
           Decrement
         </button>
         <button
-          
           className="bg-indigo-400 text-white px-3 py-2 rounded shadow"
           onClick={increment}
         >
