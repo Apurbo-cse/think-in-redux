5d2855c5e3fade851afd34f634a65731864445d0
Apurbo Kumar Anup <apurboka@gmail.com>
2023-02-24 13:14:58 +0600

    Update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-
 import Counter from "./components/Counter";
 
-function App() {
- 
-  return (
-   <>
-  
+export default function App() {
+    return (
+        <div className="w-screen h-screen p-10 bg-gray-100 text-slate-700">
+            <h1 className="max-w-md mx-auto text-center text-2xl font-bold">
+                Simple Counter Application
+            </h1>
 
-   <div className="w-screen h-screen p-10 bg-gray-100 text-slate-700">
-   <h1 className="max-w-md mx-auto text-center text2xl font-bold">
-     Simple Counter Application
-   </h1>
-   <div className="max-w-md mx-auto mt-10 space-y-5">
-     <Counter />
-     <Counter />
-   </div>
- </div>
- </>
-  );
+            <div className="max-w-md mx-auto mt-10 space-y-5">
+                <Counter />
+                <Counter />
+                <Counter />
+            </div>
+        </div>
+    );
 }
-
-export default App;
