4ff2c57241b3f8eb4e796ec1f0a372f669f61a87
Apurbo Kumar Anup <apurboka@gmail.com>
2023-03-04 10:19:08 +0600

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -7,6 +7,10 @@ const myLogger = (store) => (next) => (action) => {
     console.log(`Actions: ${JSON.stringify(action)}`)
     console.log(`Before: ${JSON.stringify(store.getState())}`)
 
+    const upcomingState = [action].reduce(rootReducer, store.getState())
+
+    console.log(`upcomingState: ${JSON.stringify(upcomingState)}`)
+
     // pass action
     return next(action);
 
