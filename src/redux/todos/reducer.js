6c2463df8a00bd217df77541aaba1d877ff1ec69
Apurbo Kumar Anup <apurboka@gmail.com>
2023-03-07 17:08:07 +0600

    Update reducer.js

--- src/redux/todos/reducer.js
+++ src/redux/todos/reducer.js
@@ -1,3 +1,62 @@
-const reducer = (state = initialState, action) => {
+import { ADDED, CLEAR_COMPLETED, COLORS_SLECTED, COMPLETED, DELETED, TOGGLED } from "./actionTypes";
+import { initialState } from "./initialState";
 
-}
+const nextTodoId = (todos) => {
+  const maxId = todos.reduce((maxId, todo) => Math.max(todo.id, maxId), -1);
+  return maxId + 1;
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADDED:
+      return [
+        ...state,
+        {
+          id: nextTodoId,
+        },
+      ];
+
+    case TOGGLED:
+      return state.map((todo) => {
+        if (todo.id !== action.payload) {
+          return todo;
+        }
+        return {
+          ...todo,
+          completed: !todo.completed,
+        };
+      });
+
+    case COLORS_SLECTED:
+      const { todoId, color } = action.payload;
+      return state.map((todo) => {
+        if (todo.id !== todoId) {
+          return todo;
+        }
+        return {
+          ...todo,
+          color: color,
+        };
+      });
+
+    case DELETED:
+      return state.filter((todo) => todo.id !== action.payload);
+
+    case COMPLETED:
+      return state.map((todo) => {
+        return {
+          ...todo,
+          completed: true,
+        };
+      });
+
+    case CLEAR_COMPLETED:
+      return state.filter((todo) => !todo.completed);
+
+    default:
+      break;
+  }
+};
+
+
+export default reducer;
