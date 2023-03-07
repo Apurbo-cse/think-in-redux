e2ba46c7c9daf9bd1cd0b40d267675c99c809447
Apurbo Kumar Anup <apurboka@gmail.com>
2023-03-07 15:43:59 +0600

    Update actionTypes.js

--- src/redux/todos/actionTypes.js
+++ src/redux/todos/actionTypes.js
@@ -1,26 +1,25 @@
-import { ADDED, COLORSSLECTED, TOGGLED } from "./actions"
+import { ADDED, COLORSSLECTED, TOGGLED } from "./actions";
 
 export const added = (todoText) => {
-    return {
-        type:ADDED,
-        payload:todoText
-    }
-}
+  return {
+    type: ADDED,
+    payload: todoText,
+  };
+};
 
 export const toggled = (todoId) => {
-    return {
-        type:TOGGLED,
-        payload:todoId
-    }
-}
+  return {
+    type: TOGGLED,
+    payload: todoId,
+  };
+};
 
-export const colorSelected = (todoId,color) => {
-    return{
-        type:COLORSSLECTED,
-        payload: {
-            todoId:todoId,
-            color:color
-        }
-    }
-}
-
+export const colorSelected = (todoId, color) => {
+  return {
+    type: COLORSSLECTED,
+    payload: {
+      todoId,
+      color,
+    },
+  };
+};
