650146d35a44ed566bb85dfa7a61b6b231791023
Apurbo Kumar Anup <apurboka@gmail.com>
2023-03-09 23:41:38 +0600

    Navbar.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
 import React from "react";
 import './../src/assets/css/output.css'
+import Navbar from "./components/Navbar";
 const App = () => {
   return (
     <div>
-      {/* <!-- Navbar --> */}
-      <nav class="bg-[#171C2A] py-4">
-        <div class="navBar">
-          <a href="index.html">
-            <img src="./images/logo.png" alt="LWS" class="max-w-[140px]" />
-          </a>
-
-          <div class="flex gap-4">
-            <a href="#home" class="navHome" id="lws-home">
-              {" "}
-              Home{" "}
-            </a>
-            <a href="cart.html" class="navCart" id="lws-cart">
-              <i class="text-xl fa-sharp fa-solid fa-bag-shopping"></i>
-              <span id="lws-totalCart">0</span>
-            </a>
-          </div>
-        </div>
-      </nav>
-      {/* <!-- Navbar ends --> */}
+     <Navbar/>
       <main class="py-16">
         <div class="productWrapper">
           {/* <!-- products container --> */}
