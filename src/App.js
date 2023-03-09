b588d650e7f1599658e96743e146a933946fed0b
Apurbo Kumar Anup <apurboka@gmail.com>
2023-03-09 23:39:55 +0600

    App

--- src/App.js
+++ src/App.js
@@ -1,30 +1,149 @@
-import { Provider } from "react-redux";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Navbar from "./components/Navbar";
-import TodoList from "./components/TodoList";
-import store from "./redux/store";
+import React from "react";
+import './../src/assets/css/output.css'
+const App = () => {
+  return (
+    <div>
+      {/* <!-- Navbar --> */}
+      <nav class="bg-[#171C2A] py-4">
+        <div class="navBar">
+          <a href="index.html">
+            <img src="./images/logo.png" alt="LWS" class="max-w-[140px]" />
+          </a>
 
-function App() {
-    return (
-        <Provider store={store}>
-            <div className="grid place-items-center bg-blue-100 h-screen px-6 font-sans">
-                <Navbar />
-
-                <div className="w-full max-w-3xl shadow-lg rounded-lg p-6 bg-white">
-                    <Header />
-
-                    <hr className="mt-4" />
-
-                    <TodoList />
-
-                    <hr className="mt-4" />
-
-                    <Footer />
+          <div class="flex gap-4">
+            <a href="#home" class="navHome" id="lws-home">
+              {" "}
+              Home{" "}
+            </a>
+            <a href="cart.html" class="navCart" id="lws-cart">
+              <i class="text-xl fa-sharp fa-solid fa-bag-shopping"></i>
+              <span id="lws-totalCart">0</span>
+            </a>
+          </div>
+        </div>
+      </nav>
+      {/* <!-- Navbar ends --> */}
+      <main class="py-16">
+        <div class="productWrapper">
+          {/* <!-- products container --> */}
+          <div class="productContainer" id="lws-productContainer">
+            {/* <!-- product item --> */}
+            <div class="lws-productCard">
+              <img
+                class="lws-productImage"
+                src="https://i.dummyjson.com/data/products/59/thumbnail.jpg"
+                alt="product"
+              />
+              <div class="p-4 space-y-2">
+                <h4 class="lws-productName">Spring and summershoes</h4>
+                <p class="lws-productCategory">Mens shoes</p>
+                <div class="flex items-center justify-between pb-2">
+                  <p class="productPrice">
+                    BDT <span class="lws-price">400</span>
+                  </p>
+                  <p class="productQuantity">
+                    QTY <span class="lws-quantity">10</span>
+                  </p>
                 </div>
+                <button class="lws-btnAddToCart">Add To Cart</button>
+              </div>
             </div>
-        </Provider>
-    );
-}
+            {/* <!-- product item ends --> */}
+
+            {/* <!-- product item --> */}
+            <div class="lws-productCard">
+              <img
+                class="lws-productImage"
+                src="https://i.dummyjson.com/data/products/40/thumbnail.jpg"
+                alt="product"
+              />
+              <div class="p-4 space-y-2">
+                <h4 class="lws-productName">Women Winter Clothes</h4>
+                <p class="lws-productCategory">Tops</p>
+                <div class="flex items-center justify-between pb-2">
+                  <p class="productPrice">
+                    BDT <span class="lws-price">100</span>
+                  </p>
+                  <p class="productQuantity">
+                    QTY <span class="lws-quantity">30</span>
+                  </p>
+                </div>
+                <button class="lws-btnAddToCart">Add To Cart</button>
+              </div>
+            </div>
+            {/* <!-- product item ends --> */}
+          </div>
+          {/* <!-- products container ends --> */}
+          <div>
+            {/* <!-- Product Input Form --> */}
+            <div class="formContainer">
+              <h4 class="formTitle">Add New Product</h4>
+              <form class="space-y-4 text-[#534F4F]" id="lws-addProductForm">
+                {/* <!-- product name --> */}
+                <div class="space-y-2">
+                  <label for="lws-inputName">Product Name</label>
+                  <input
+                    class="addProductInput"
+                    id="lws-inputName"
+                    type="text"
+                    required
+                  />
+                </div>
+                {/* <!-- product category --> */}
+                <div class="space-y-2">
+                  <label for="lws-inputCategory">Category</label>
+                  <input
+                    class="addProductInput"
+                    id="lws-inputCategory"
+                    type="text"
+                    required
+                  />
+                </div>
+                {/* <!-- product image url --> */}
+                <div class="space-y-2">
+                  <label for="lws-inputImage">Image Url</label>
+                  <input
+                    class="addProductInput"
+                    id="lws-inputImage"
+                    type="text"
+                    required
+                  />
+                </div>
+                {/* <!-- price & quantity container --> */}
+                <div class="grid grid-cols-2 gap-8 pb-4">
+                  {/* <!-- price --> */}
+                  <div class="space-y-2">
+                    <label for="ws-inputPrice">Price</label>
+                    <input
+                      class="addProductInput"
+                      type="number"
+                      id="lws-inputPrice"
+                      required
+                    />
+                  </div>
+                  {/* <!-- quantity --> */}
+                  <div class="space-y-2">
+                    <label for="lws-inputQuantity">Quantity</label>
+                    <input
+                      class="addProductInput"
+                      type="number"
+                      id="lws-inputQuantity"
+                      required
+                    />
+                  </div>
+                </div>
+                {/* <!-- submit button --> */}
+                <button type="submit" id="lws-inputSubmit" class="submit">
+                  Add Product
+                </button>
+              </form>
+            </div>
+            {/* <!-- Product Input Form Ends --> */}
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+};
 
 export default App;
