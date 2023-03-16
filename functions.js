d9b657a3c6a9cf03a9e8136950576d61519c193e
Apurbo Kumar Anup <apurboka@gmail.com>
2023-03-16 11:51:35 +0600

    functions.js

---
+++ functions.js
@@ -0,0 +1,20 @@
+const fetchTodos = async () => {
+    const response = await fetch(
+        "https://jsonplaceholder.typicode.com/todos?_limit=5"
+    );
+
+    const todos = await response.json();
+
+    store.dispatch({
+        type: "todos/todoLoaded",
+        payload: todos,
+    });
+
+    console.log(
+        `Number of updated todos: ${store.getState().todos.length}`
+    );
+
+    return;
+}
+
+module.exports = fetchTodos;
