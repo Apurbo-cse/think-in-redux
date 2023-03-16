5a935aad643e1125fe0cc1ccd3704177563857ff
Apurbo Kumar Anup <apurboka@gmail.com>
2023-03-16 10:37:56 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,48 @@
+const {createStore, applyMiddleware} = require('redux');
+
 // initial State
 const initialState = {
   todos: [],
 };
+
+// reducer
+const todoReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "todos/todoAdded":
+      return {
+        ...state,
+        todos: [
+          ...state.todos,
+          {
+            title: action.payload,
+          },
+        ],
+      };
+
+    case "todos/todoLoaded":
+      return {
+        ...state,
+        todos: [...state.todos, ...action.payload],
+      };
+
+    default:
+      break;
+  }
+};
+
+// store
+const store = createStore(todoReducer, applyMiddleware());
+
+
+// subscribe to state changes
+ store.subscribe(() => {
+    console.log(store.getState())
+ })
+
+
+ // dispatch action
+ store.dispatch({
+    type: "todos/todoAdded",
+    payload: "Learn Redux",
+ })
+
