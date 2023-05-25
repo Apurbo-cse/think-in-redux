a75ca66e8a16ec888af885ab702d5eb5216b44e5
Apurbo Kumar Anup <apurboka@gmail.com>
2023-05-25 10:42:46 +0600

    Create reducer.js

---
+++ src/redux/books/reducer.js
@@ -0,0 +1,25 @@
+import initialState from "./initialState";
+
+const nextBookId = (books) => {
+    const maxId = books.reduce((maxId, book) => Math.max(book.id, maxId), -1);
+    return maxId + 1;
+};
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case ADDED:
+            return [
+                ...state,
+                {
+                    id: nextBookId(state),
+                    text:action.payload,
+                    feature: false,
+                },
+            ];
+        
+        default:
+            return state;    
+    }
+}
+
+export default reducer;
