2a77f8e0d3cefc481098ffe3aad9156f5283246e
Apurbo Kumar Anup <apurboka@gmail.com>
2023-05-25 10:30:09 +0600

    Create BookList.js

---
+++ src/components/BookList.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { BookCard } from "./BookCard";
+
+const BookList = () => {
+  return (
+    <>
+      <div class="flex items-center justify-between mb-12">
+        <h4 class="mt-2 text-xl font-bold">Book List</h4>
+
+        <div class="flex items-center space-x-4">
+          <button class="filter-btn active-filter" id="lws-filterAll">
+            All
+          </button>
+          <button class="filter-btn" id="lws-filterFeatured">
+            Featured
+          </button>
+        </div>
+      </div>
+
+      <div class="lws-bookContainer">
+        <BookCard/>
+      </div>
+      
+    </>
+  );
+};
+
+export default BookList;
