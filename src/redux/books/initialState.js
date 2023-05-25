776cf18d64ca4633aa5ab2fe29fca6354ca239b6
Apurbo Kumar Anup <apurboka@gmail.com>
2023-05-25 10:38:57 +0600

    Create initialState.js

---
+++ src/redux/books/initialState.js
@@ -0,0 +1,21 @@
+const initialState = [
+    {
+        id: 1,
+        ittle: "Learn React JS",
+        author: " abc",
+        img:"",
+        price: 100,
+        rating: 5,
+        feature: true,
+    },
+    {
+        id: 2,
+        ittle: "Learn Redux",
+        author: " abc",
+        img:"",
+        price: 100,
+        rating: 5,
+    },
+];
+
+export default initialState;
